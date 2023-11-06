dc2812bce644a19edf0c7305723737a90411803a
Ian_França <iiann_2009@hotmail.com>
2023-11-06 14:25:59 -0300

    Desafio, iniciando

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,10 +1,13 @@
-import contador from './contador.js';
-
-const reducer = Redux.combineReducers({ contador });
-
-const store = Redux.createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
-
-export default store;
+// Usando o Redux (pode usar Immer ou Não).
+// Crie uma store contendo os estados iniciais abaixo
+// Crie as seguintes ações:
+// aluno/INCREMENTAR_TEMPO, adiciona 1 dia de acesso
+// aluno/REDUZIR_TEMPO, reduz 1 dia de acesso
+// aluno/MODIFICAR_EMAIL(email), modifica o email do usuário
+// aulas/COMPLETAR_AULA(id), completa a aula com base no ID passado
+// aulas/COMPLETAR_CURSO, completa todas as aulas
+// aulas/RESETAR_CURSO, reseta todas as aulas completas
+// Crie constantes e action creators para as ações.
+// Crie um reducer para aluno e um para aulas.
+// Renderize na tela o nome, email, tempo restante e o total de aulas completas
+// Configure a DevTools
--- store/contador.js
+++ store/contador.js
@@ -1,25 +1,67 @@
-// contador.js
-// Constant
-const INCREMENTAR = 'contador/INCREMENTAR';
-const REDUZIR = 'contador/REDUZIR';
+// Usando o Redux (pode usar Immer ou Não).
+// Crie uma store contendo os estados iniciais abaixo
+// Crie as seguintes ações:
+// aluno/INCREMENTAR_TEMPO, adiciona 1 dia de acesso
+// aluno/REDUZIR_TEMPO, reduz 1 dia de acesso
+// aluno/MODIFICAR_EMAIL(email), modifica o email do usuário
+// aulas/COMPLETAR_AULA(id), completa a aula com base no ID passado
+// aulas/COMPLETAR_CURSO, completa todas as aulas
+// aulas/RESETAR_CURSO, reseta todas as aulas completas
+// Crie constantes e action creators para as ações.
+// Crie um reducer para aluno e um para aulas.
+// Renderize na tela o nome, email, tempo restante e o total de aulas completas
+// Configure a DevTools
 
-// Action Creator
-export const incrementar = () => ({ type: INCREMENTAR });
-export const reduzir = () => ({ type: REDUZIR });
-
-// Initial State
-const initialState = 0;
-
-// Reducer
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case INCREMENTAR:
-      return state + 1;
-    case REDUZIR:
-      return state - 1;
-    default:
-      return state;
-  }
+const aluno = {
+  nome: 'André Rafael',
+  email: 'andre@origamid.com',
+  diasRestantes: 120,
 };
 
-export default reducer;
+const aulas = [
+  {
+    id: 1,
+    nome: 'Design',
+    completa: true,
+  },
+  {
+    id: 2,
+    nome: 'HTML',
+    completa: false,
+  },
+  {
+    id: 3,
+    nome: 'CSS',
+    completa: false,
+  },
+  {
+    id: 4,
+    nome: 'JavaScript',
+    completa: false,
+  },
+];
+
+const incrementarTempo = () => ({ type: 'aluno/INCREMENTAR_TEMPO' });
+const reduzirTempo = () => ({ type: 'aluno/REDUZIR_TEMPO' });
+const modificarEmail = (email) => ({
+  type: 'aluno/MODIFICAR_EMAIL',
+  payload: email,
+});
+const completarAula = (id) => ({ type: id });
+const completarCurso = () => ({ type: 'aluno/COMPLETAR_CURSO' });
+const resetarCurso = () => ({ type: 'aluno/RESETAR  _CURSO' });
+
+const contadorTempo = (state = aluno.tempo, action) => {
+  switch (action.type) {
+    case incrementarTempo():
+      state + 1;
+      break;
+    case reduzirTempo():
+      state - 1;
+      break;
+
+    default:
+      state;
+      break;
+  }
+};
