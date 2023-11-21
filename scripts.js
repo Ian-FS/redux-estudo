1e1b2f9ba5e98a320dd4c3da89b4745fc72e8d0f
Ian_França <iiann_2009@hotmail.com>
2023-11-20 23:25:58 -0300

    Middleware: Configurando  middleware

--- scripts.js
+++ scripts.js
@@ -10,9 +10,20 @@ function reducer(state = 0, action) {
   }
 }
 
-const logger = (store) => (next) => (action) => next(action);
+const { applyMiddleware, compose } = Redux;
 
-const middleware = Redux.applyMddleware(logger);
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.log('ACTION', action);
+  console.log('PREV_STATE', store.getState());
+  const result = next(action);
+  console.log('NEW_STATE', store.getState());
+  console.groupEnd();
+
+  return result;
+};
+
+const middleware = Redux.applyMiddleware(logger);
 
 const store = Redux.createStore(reducer, middleware);
 
